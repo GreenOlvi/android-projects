c5c0fed02ff34b6dcc8bbea76cedd538e7e075d6
Piotr Szulc <szulo123@gmail.com>
2012-07-24 10:41:03 +0200

    Initial commit for Android version of Game

---
+++ AndroidGame/src/com/greenolvi/android/game/AndroidGame.java
@@ -0,0 +1,13 @@
+package com.greenolvi.android.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidGame extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
