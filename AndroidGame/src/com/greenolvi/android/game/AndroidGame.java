54dbf5ed757e2b57b78243c4ccf433e3c645e840
Piotr Szulc <szulo123@gmail.com>
2012-08-10 09:48:08 +0200

    * Changed View to SurfaceView  * Early tile drawing

--- AndroidGame/src/com/greenolvi/android/game/AndroidGame.java
+++ AndroidGame/src/com/greenolvi/android/game/AndroidGame.java
@@ -2,6 +2,8 @@ package com.greenolvi.android.game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.greenolvi.android.game.gfx.ScreenView;
 
@@ -14,8 +16,26 @@ public class AndroidGame extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         mScreenView = new ScreenView(this);
         
         setContentView(mScreenView);
     }
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mScreenView.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mScreenView.onResume();
+	}
+    
+    
+    
 }
--- AndroidGame/src/com/greenolvi/android/game/gfx/ScreenView.java
+++ AndroidGame/src/com/greenolvi/android/game/gfx/ScreenView.java
@@ -1,44 +1,123 @@
 package com.greenolvi.android.game.gfx;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.AttributeSet;
-import android.view.View;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class ScreenView extends View {
+import com.greenolvi.android.game.R;
 
+public class ScreenView extends SurfaceView implements Runnable {
+	
+	Thread thread = null;
+	SurfaceHolder surfaceHolder;
+	volatile boolean running = false;
+	
 	public ScreenView(Context context) {
 		super(context);
+		surfaceHolder = getHolder();
 	}
 
-	public ScreenView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
-
-	public ScreenView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
-	
-	private Paint mPaint;
-	
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		if (mPaint == null) {
-			mPaint = new Paint();
-			mPaint.setColor(Color.WHITE);
+	public void onPause() {
+		boolean retry = true;
+		running = false;
+		while (retry) {
+			try {
+				thread.join();
+				retry = false;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-		canvas.drawColor(Color.BLACK);
-		for (int y = 0; y < canvas.getHeight(); y++) {
-			for (int x = 0; x < canvas.getWidth(); x++) {
-				if ((x & y) == 0) {
-					canvas.drawPoint(x, y, mPaint);
-				}
+	}
+
+	public void onResume() {
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+
+	@Override
+	public void run() {
+		while (running) {
+			if (surfaceHolder.getSurface().isValid()) {
+				Canvas canvas = surfaceHolder.lockCanvas();
+				
+				drawBackground(canvas);
+				
+				surfaceHolder.unlockCanvasAndPost(canvas);
 			}
 		}
 	}
 	
+	private Bitmap tiles = BitmapFactory.decodeResource(getResources(), R.drawable.icons);
+	
+	int xOffset = 0;
+	int yOffset = 0;
+	
+	private boolean touching = false;
+	
+	private void drawBackground(Canvas canvas) {
+		Paint paint = new Paint();
+		paint.setColor(Color.WHITE);
+		
+		canvas.drawColor(Color.BLACK);
+		for (int y = 0; y < canvas.getHeight(); y++) {
+			for (int x = 0; x < canvas.getWidth(); x++) {
+				if ((x & y) == 0) {
+					int b = touching ? 200 : 0;
+					paint.setColor(Color.rgb((x & 0xFF), (y & 0xFF), b));
+					canvas.drawPoint(x, y, paint);
+				}
+			}
+		}
+		
+		render(canvas, 100, 100, 0);
+		render(canvas, 200, 100, 32);
+		render(canvas, 300, 100, 10);
+	}
+	
+	public void render(Canvas canvas, int xp, int yp, int tile) {
+		xp -= xOffset;
+		yp -= yOffset;
+		
+		int xTile = tile % 32;
+		int yTile = tile / 32;
+		
+		canvas.drawBitmap(tiles, new Rect(xTile * 8 - 2, yTile * 8, xTile * 8 + 8 -2, yTile * 8 + 8), new Rect(xp, yp, xp + 8 * 5, yp + 8 * 5), null);
+	}
+	
+	public void setOffset(int xOffset, int yOffset) {
+		this.xOffset = xOffset;
+		this.yOffset = yOffset;
+	}
+	
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		int action = event.getAction();
+		switch(action) {
+		case MotionEvent.ACTION_DOWN:
+			touching = true;
+			break;
+		case MotionEvent.ACTION_UP:
+			touching = false;
+			break;
+		case MotionEvent.ACTION_CANCEL:
+			touching = false;
+			break;
+		case MotionEvent.ACTION_OUTSIDE:
+			touching = false;
+			break;
+		}
+		this.invalidate();
+		return true;
+	}
 
 }
